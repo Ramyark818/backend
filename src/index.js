dd2a490d59208bdb58335fbcb5f974c700011190
Ramya <ramyark818@gmail.com>
2025-04-02 21:55:15 +0530

    Router andcontroller (&) add register controller and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import express from "express"
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./.env'
-})
+});
+
+// const app=express();
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
+    app.listen(process.env.PORT || 3000, ()=>{
         console.log(`Server is running at:${process.env.PORT}`);
-    })
+    });
 })
 .catch((err)=>{
     console.log("MONGO db connection failed !!!",err)
